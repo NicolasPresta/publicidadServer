6790df5cf0d6549d09056a87514815a1b3bf376a
Nicolás Presta <presta_26@hotmail.com>
2016-03-14 03:05:00 -0300

    agrego más dummys

--- src/lib/publicidadManager.js
+++ src/lib/publicidadManager.js
@@ -6,7 +6,7 @@ var dummy = { publicidades:
 		{	
 			titulo:"Una super oferta",
 			descripcion:"TV 42 pulgadas a solo $10000",
-			img: "http://production-alkosto-data.s3-website-us-east-1.amazonaws.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/t/v/tv-42-led-lg-42ln5700-fhd-int-5.jpg",
+			img: "http://cdn.arstechnica.net/wp-content/uploads/2012/09/steambigpic.png",
 			vigenciaDesde: "09/03/2016",
 			vigenciaHasta: "30/03/2016",
 			condiciones: "solo si tenes familia en la antartida"
@@ -15,6 +15,54 @@ var dummy = { publicidades:
 			titulo:"Ahora más barato!",
 			descripcion:"juego de living 3 piezas",
 			img: "http://mueblerialaeconomia.net/img/productos/juego_de_living_lore.jpg",
+			vigenciaDesde: "01/03/2016",
+			vigenciaHasta: "15/03/2016",
+			condiciones: "no incluye los sillones"
+		},
+		{
+			titulo:"Pecas en oferta",
+			descripcion:"Solo por hoy",
+			img: "http://www.ellashoy.com/images/pecas-en-la-cara.jpg",
+			vigenciaDesde: "01/03/2016",
+			vigenciaHasta: "15/03/2016",
+			condiciones: "no incluye los sillones"
+		},
+		{
+			titulo:"Regalo del día del perro",
+			descripcion:"Aprovecha y poné contento al boby",
+			img: "http://img.mundoperros.es/wp-content/uploads/2012/02/perros1.jpeg",
+			vigenciaDesde: "01/03/2016",
+			vigenciaHasta: "15/03/2016",
+			condiciones: "no incluye los sillones"
+		},
+		{
+			titulo:"El living de un rey",
+			descripcion:"Stock re limitado",
+			img: "http://rossidisegno.com/objects/wp-content/uploads/2013/07/A_MesaRatonaBodega_E0000.jpg",
+			vigenciaDesde: "01/03/2016",
+			vigenciaHasta: "15/03/2016",
+			condiciones: "no incluye los sillones"
+		},
+		{
+			titulo:"Encontrá el look para el otoño",
+			descripcion:"Vas a quedar hermosa y otoñal",
+			img: "http://i2.wp.com/www.fabricastextiles.com.ar/wp-content/uploads/2014/04/fabrica-de-camperas-de-cuero.jpg",
+			vigenciaDesde: "01/03/2016",
+			vigenciaHasta: "15/03/2016",
+			condiciones: "no incluye los sillones"
+		},
+		{
+			titulo:"La mejor promoción en lentes",
+			descripcion:"Los que usá el diego",
+			img: "http://www.opticapaesani.com.ar/media/catalog/product/cache/8/image/9df78eab33525d08d6e5fb8d27136e95/t/h/the_guardian_opaco.jpg",
+			vigenciaDesde: "01/03/2016",
+			vigenciaHasta: "15/03/2016",
+			condiciones: "no incluye los sillones"
+		},
+		{
+			titulo:"Pizzara para los más chicos",
+			descripcion:"Aprenden jugando y es barato",
+			img: "http://static.imujer.com/sites/default/files/lasmanualidades/pizarra.jpg",
 			vigenciaDesde: "01/03/2016",
 			vigenciaHasta: "15/03/2016",
 			condiciones: "no incluye los sillones"
